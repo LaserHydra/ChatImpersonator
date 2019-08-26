00a22fb28bec95aea10a010035458d206ffc37cd
Oliver <contact@laserhydra.com>
2019-08-26 20:57:39 +0200

    Replace tabs with spaces

--- ChatImpersonator.cs
+++ ChatImpersonator.cs
@@ -8,44 +8,44 @@ namespace Oxide.Plugins
     [Description("Allows you to impersonate another player in chat")]
     public class ChatImpersonator : CovalencePlugin
     {
-	    private const string Permission = "chatimpersonator.use";
+        private const string Permission = "chatimpersonator.use";
 
-		protected override void LoadDefaultMessages()
-	    {
-			lang.RegisterMessages(new Dictionary<string, string>
-			{
-				["Syntax"] = "Syntax: /ci <player> <message>",
-				["Player Not Found"] = "The player could not be found."
-			}, this);
-	    }
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Syntax"] = "Syntax: /ci <player> <message>",
+                ["Player Not Found"] = "The player could not be found."
+            }, this);
+        }
 
-	    [Command("ci"), Permission(Permission)]
+        [Command("ci"), Permission(Permission)]
         private void Cmd_Impersonate(IPlayer player, string command, string[] args)
         {
             if (args.Length < 2)
             {
-	            SendLocalizedReply(player, "Syntax");
+                SendLocalizedReply(player, "Syntax");
                 return;
             }
-			
+            
             var targetPlayer = FindPlayer(args[0]);
 
-	        if (targetPlayer == null)
-	        {
-		        SendLocalizedReply(player, "Player Not Found");
-		        return;
-	        }
+            if (targetPlayer == null)
+            {
+                SendLocalizedReply(player, "Player Not Found");
+                return;
+            }
 
-	        ForcePlayerChat(targetPlayer, string.Join(" ", args.Skip(1).ToArray()));
+            ForcePlayerChat(targetPlayer, string.Join(" ", args.Skip(1).ToArray()));
         }
 
-	    private void ForcePlayerChat(BasePlayer target, string message) =>
-		    target.SendConsoleCommand($"chat.say \"{message}\"");
+        private void ForcePlayerChat(BasePlayer target, string message) =>
+            target.SendConsoleCommand($"chat.say \"{message}\"");
 
-	    private void SendLocalizedReply(IPlayer player, string key) => 
-			player.Reply(lang.GetMessage(key, this, player.Id));
+        private void SendLocalizedReply(IPlayer player, string key) => 
+            player.Reply(lang.GetMessage(key, this, player.Id));
 
-	    private BasePlayer FindPlayer(string nameOrId) =>
+        private BasePlayer FindPlayer(string nameOrId) =>
             covalence.Players.FindPlayer(nameOrId)?.Object as BasePlayer;
     }
 }
